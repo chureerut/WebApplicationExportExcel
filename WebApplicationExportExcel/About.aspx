898ff85a1348cdd9972d6f1457bc531bf264ae5f
SGN01 <SGN01@LAPTOP-N3Q9D4DB>
2024-02-04 13:06:30 +0700

    Add project files.

---
+++ WebApplicationExportExcel/About.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApplicationExportExcel.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information.
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:Button ID="ButtonExport" runat="server" OnClick="ButtonExport_Click" Text="Export To Excel" />
+        <table style="width:100%;">
+            <tr>
+                <td>
+                    <asp:TextBox ID="firstvalue" runat="server"></asp:TextBox>
+                </td>
+                
+            </tr>
+            <tr>
+                <td style="height: 22px">
+                    <asp:TextBox ID="secondvalue" runat="server"></asp:TextBox>
+                </td>
+                
+            </tr>
+             <tr>
+                <td>
+                    <asp:TextBox ID="operation" runat="server"></asp:TextBox>
+                 </td>
+                
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="Button2" runat="server" Text="Button" OnClick="Button2_Click" />
+                </td>
+            
+            </tr>
+            <tr>
+                <td>
+                    
+                    <asp:Label ID="lbresult" runat="server" Text="Label"></asp:Label>
+                </td>
+                
+            </tr>
+        </table>
+    </p>
+</asp:Content>
---
+++ WebApplicationExportExcel/About.aspx.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.IO;
+using WebApplicationExportExcel.ServiceReference1;
+
+namespace WebApplicationExportExcel
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var students = new[]{
+                               new{ Id="101", Name="Vivek", Address="Hyderabad" },
+                               new{ Id="102", Name="Ranjeet", Address="Hyderabad" },
+                               new{ Id="103", Name="Sharath", Address="Hyderabad" },
+                               new{ Id="104", Name="Ganesh", Address="Hyderabad" },
+                               new{ Id="105", Name="Gajanan", Address="Hyderabad" },
+                               new{ Id="106", Name="Ashish", Address="Hyderabad" }
+                      };
+
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Sheet1");
+
+
+            workSheet.TabColor = System.Drawing.Color.Black;
+            workSheet.DefaultRowHeight = 12;
+
+
+            //Header of table
+            //
+            workSheet.Row(1).Height = 20;
+            workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            workSheet.Row(1).Style.Font.Bold = true;
+
+            workSheet.Cells[1, 1].Value = "S.No";
+            workSheet.Cells[1, 2].Value = "Id";
+            workSheet.Cells[1, 3].Value = "Name";
+            workSheet.Cells[1, 4].Value = "Address";
+
+            //Body of table
+            //
+            int recordIndex = 2;
+
+            foreach (var student in students)
+            {
+                workSheet.Cells[recordIndex, 1].Value = (recordIndex - 1).ToString();
+                workSheet.Cells[recordIndex, 2].Value = student.Id;
+                workSheet.Cells[recordIndex, 3].Value = student.Name;
+                workSheet.Cells[recordIndex, 4].Value = student.Address;
+
+                recordIndex++;
+            }
+
+            workSheet.Column(1).AutoFit();
+            workSheet.Column(2).AutoFit();
+            workSheet.Column(3).AutoFit();
+            workSheet.Column(4).AutoFit();
+
+            string excelName = "studentsRecord";
+
+            using (var memoryStream = new MemoryStream())
+            {
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;  filename=" + excelName + ".xlsx");
+                excel.SaveAs(memoryStream);
+                memoryStream.WriteTo(Response.OutputStream);
+                Response.Flush();
+                Response.End();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ServiceReference1.WebService1SoapClient client = new ServiceReference1.WebService1SoapClient();
+                string result = client.Calculator(Convert.ToInt32(firstvalue.Text), Convert.ToInt32(secondvalue.Text), operation.Text);
+                if (result != "")
+                    lbresult.Text = "Result" + result.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());  // Or Debug.Print, or whatever
+                throw; // So exception propagation will continue
+            }
+            
+        }
+
+        protected void ButtonExport_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient client = new ServiceReference1.WebService1SoapClient();
+            string gender = Convert.ToString("Female");
+
+            var result3 = client.Products(gender);
+            List<Product> products = new List<Product>();
+            foreach (object obj in result3)
+                products.Add((Product)obj);
+
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("ProductList");
+
+            workSheet.TabColor = System.Drawing.Color.Black;
+            workSheet.DefaultRowHeight = 12;
+
+            //Header of table
+            //
+            workSheet.Row(1).Height = 20;
+            workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            workSheet.Row(1).Style.Font.Bold = true;
+
+            workSheet.Cells[1, 1].Value = "Name";
+            workSheet.Cells[1, 2].Value = "LastName";
+            workSheet.Cells[1, 3].Value = "Gender";
+
+            //Body of table
+            //
+            int recordIndex = 2;
+
+            foreach (var productitem in products)
+            {
+                workSheet.Cells[recordIndex, 1].Value = productitem.Name;
+                workSheet.Cells[recordIndex, 2].Value = productitem.LastName;
+                workSheet.Cells[recordIndex, 3].Value = productitem.Gender;
+
+                recordIndex++;
+            }
+
+            workSheet.Column(1).AutoFit();
+            workSheet.Column(2).AutoFit();
+            workSheet.Column(3).AutoFit();
+
+            string excelName = "ProductsRecord";
+
+            using (var memoryStream = new MemoryStream())
+            {
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;  filename=" + excelName + ".xlsx");
+                excel.SaveAs(memoryStream);
+                memoryStream.WriteTo(Response.OutputStream);
+                Response.Flush();
+                Response.End();
+            }
+        }
+    }
+}
